6ecc56016ef409bbb12477b032695258db463eb6
Raymundo Solis <pepperray01@gmail.com>
2020-09-06 21:26:13 -0500

    Se agregan retos 1,2,3 y 4

--- src/index.js
+++ src/index.js
@@ -1,38 +1,58 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://rickandmortyapi.com/api/character/';
+const $app = document.getElementById("app");
+const $observe = document.getElementById("observe");
+const API = "https://rickandmortyapi.com/api/character/";
+const localStorage = window.localStorage;
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
+const getData = async (api) => {
+  await fetch(api)
+    .then((response) => response.json())
+    .then((response) => {
       const characters = response.results;
-      let output = characters.map(character => {
-        return `
+      let output = characters
+        .map((character) => {
+          return `
       <article class="Card">
         <img src="${character.image}" />
         <h2>${character.name}<span>${character.species}</span></h2>
       </article>
-    `
-      }).join('');
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
+    `;
+        })
+        .join("");
+      localStorage.setItem("next_fetch", response.info.next);
+      let newItem = document.createElement("section");
+      newItem.classList.add("Items");
       newItem.innerHTML = output;
       $app.appendChild(newItem);
     })
-    .catch(error => console.log(error));
-}
+    .catch((error) => console.log(error));
+};
 
-const loadData = () => {
-  getData(API);
-}
-
-const intersectionObserver = new IntersectionObserver(entries => {
-  if (entries[0].isIntersecting) {
-    loadData();
+const loadData = async () => {
+  var newUrl = localStorage.getItem("next_fetch");
+  if (!newUrl) {
+    await getData(API);
+  } else {
+    if (newUrl === "null") {
+      let hr = document.createElement("hr");
+      $app.appendChild(hr);
+      let finalItem = document.createElement("p");
+      finalItem.innerHTML = "No hay mas elementos";
+      $app.appendChild(finalItem);
+    } else {
+      await getData(newUrl);
+    }
   }
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+};
 
+const intersectionObserver = new IntersectionObserver(
+  (entries) => {
+    if (entries[0].isIntersecting) {
+      loadData();
+    }
+  },
+  {
+    rootMargin: "0px 0px 100% 0px",
+  }
+);
+localStorage.removeItem("next_fetch");
 intersectionObserver.observe($observe);
